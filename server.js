38b20dd2401e5e794e1752ca85d94264f25f57c9
cfmetweetme-JAM <cfmetweetme@gmail.com>
2026-04-30 15:28:21 -0400

    Change product endpoint to specific product ID

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/', (req, res) => {
 app.get('/debug-products', async (req, res) => {
   try {
     const response = await axios.get(
-      `${PRINTFUL_BASE_URL}/store/products`,
+     `${PRINTFUL_BASE_URL}/store/products/430534788`,
       { headers: printfulHeaders }
     );
     res.json(response.data);
